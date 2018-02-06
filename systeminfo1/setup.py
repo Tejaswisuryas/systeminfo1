4503aec4e54c1c6c8c97519f386af92a29bd442d
TejaswiSuryas <tejaswi.suryas@gmail.com>
2018-02-06 10:10:35 +0000

    newtest

---
+++ systeminfo1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+setup(name="systeminfo1",
+      version="0.1",
+      description="Basic system infromation for comp30670",
+      url="",
+      author="Tejaswi Suryas",
+      author_email="tejaswi.suryas@ucdconnect.ie",
+      license="GPL3",
+      packages=['systeminfo1'],
+      entry_points={
+          'console_scripts':['comp30670_systeminfo1=systeminfo1.main:main']
+          }
+      )
+      
---
+++ systeminfo1/systeminfo1/main.py
@@ -0,0 +1,12 @@
+'''
+Created on 06-Feb-2018
+
+@author: TejaswiS
+'''
+import platform
+
+def main():
+    print(platform.platform())
+    return
+if __name__ == '__main__':
+    main()
